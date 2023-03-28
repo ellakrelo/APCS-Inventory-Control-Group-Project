a7c548a3f0ab27085017de4964c76ca43d436ec8
ella.krelovich <ella.krelovich@MU-C1-11.mullen.pvt.k12.co.us>
2023-03-28 13:16:57 -0600

    Completed main menu and added classes for Customer and Employee functions. You can also only access the employee functions with a password.

--- APCS Inventory Control Group Project/src/InventoryRunner.java
+++ APCS Inventory Control Group Project/src/InventoryRunner.java
@@ -1,11 +1,74 @@
+import java.util.*;
 
 public class InventoryRunner
 	{
 
+		static Scanner userInput = new Scanner(System.in);
+		static Scanner userIntInput = new Scanner(System.in);
+		static int intInput = 0;
+		static String input = "";
+		static String password = "totallyStrongPassword";
+		
 		public static void main(String[] args)
 			{
-				//runner 
-				System.out.println("Test");
+				select();
 			}
+		
+		public static void select()
+		{
+			System.out.println("Welcome! Are you a customer or an employee?");
+			System.out.println("\t 1) Customer");
+			System.out.println("\t 2) Employee");
+			intInput = userIntInput.nextInt();
+			
+			if(intInput == 1)
+				{
+					//customer selection
+				}
+			else if(intInput == 2)
+				{
+					password();
+				}
+			else
+				{
+					System.out.println("That was not an acceptable answer");
+					input = userInput.nextLine();
+					select();
+				}
+			
+			
+		}
+		
+		public static void password()
+		{
+			System.out.println("Please enter your password");
+			input = userInput.nextLine();
+			
+			if(password.equals(input))
+				{
+					//employee selection
+				}
+			else
+				{
+					System.out.println("That was not correct. Would you like to try again?\n\t 1) Yes\n\t 2) No");
+					intInput = userIntInput.nextInt();
+					
+					if(intInput == 1)
+						{
+							password();
+						}
+					else if(intInput == 2)
+						{
+							select();
+						}
+					else
+						{
+							System.out.println("That was not an acceptable answer");
+							input = userInput.nextLine();
+							select();
+						}
+
+				}
+		}
 
 	}
