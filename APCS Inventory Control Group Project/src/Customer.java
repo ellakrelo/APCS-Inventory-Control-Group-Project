97fe7bc98ef973c65d7ed5a5ec8b38993c109419
MatthewEgan05 <matthew.egan@MU-C1-04.mullen.pvt.k12.co.us>
2023-03-29 13:46:27 -0600

    Customer - search catalog

--- APCS Inventory Control Group Project/src/Customer.java
+++ APCS Inventory Control Group Project/src/Customer.java
@@ -1,5 +1,36 @@
+import java.util.Scanner;
 
 public class Customer
 	{
-
+		static Scanner userInput = new Scanner(System.in);
+		
+		public void inventory()
+			{
+				System.out.println("Current Inventory: ");
+				System.out.println();
+				
+				// reads text file
+				
+				System.out.println("Search: ");
+				String search = userInput.nextLine();
+				searchCatalog(search);
+			}
+		
+		public void searchCatalog(String word)
+			{
+				String temp = "cat";
+				String temp1 = "catalog";
+				String temp2 = "vacation diary";
+				
+				for(int i = 0; i < word.length(); i++)
+					{
+						if(word.substring(i+1) != null && word.substring(i+2) != null) 
+							{
+								
+							}
+					}
+			}
+		
+	
+		
 	}
